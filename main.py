c59cdb5deac2d2750ea4dfccbb07a2c6e901bfcf
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-19 19:21:32 -0600

    html added

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ class MainHandler(webapp2.RequestHandler):
 
     def get(self):
 
-        header =  " <h2><div id='name'><ul><li>Sagal Logistics LLC</li><li>Sign In</li></ul></div></h2>"
+        header =  "<!DOCTYPE html><html><head></head><title>Sagal Logistics LLC Signup</title><style><link type='text/css' rel='stylesheet' href='stylesheet.css'></style><body><h2><div id='name'><ul><li>Sagal Logistics LLC</li><li>Sign In</li></ul></div></h2>"
         contentOne= "<div id='chooser'><strong>Process Load Payment</strong></div>"
-        contentTwo =  "<form method='post'><div id='forms'><ul class='operation' style='list-style-type:none'><li>Owner-Operator</li><li>Driver</li></ul></div><div id='forms1'><ul class='amount' style='list-style-type:none'><li>Date<form method='post'><br><input type='number'></form></li><br><li>Load Number<form method='post'><input type='number'></form></li><br><li>Description<form method='post'><input type='number'></form></li><br><li>Line Haul<form method='post'><input type='number' name='lineHaul'></form></li><br><li>Fuel Advance<form method='post'><input type='number' name='fuelAdvance'></form></li><br><form><input type='Submit'></form></li><br></ul><div id='net'>Net Pay<form method='post'><input type='number'></form></div></form>"
+        contentTwo =  "<form method='post'><div id='forms'><ul class='operation' style='list-style-type:none'><li>Owner-Operator</li><li>Driver</li></ul></div><div id='forms1'><ul class='amount' style='list-style-type:none'><li>Date<form method='post'><br><input type='number'></form></li><br><li>Load Number<form method='post'><input type='number'></form></li><br><li>Description<form method='post'><input type='number'></form></li><br><li>Line Haul<form method='post'><input type='number' name='lineHaul'></form></li><br><li>Fuel Advance<form method='post'><input type='number' name='fuelAdvance'></form></li><br><form><input type='Submit'></form></li><br></ul><div id='net'>Net Pay<form method='post'><input type='number'></form></div></form></body></html>"
         self.response.write( header + contentOne + contentTwo)
     def post(self):
         lineHaul = int(self.request.get("lineHaul"))
